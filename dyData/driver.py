38375f9949928ebe7bd4b7ad18adfc75573af103
lianh_mac_7F <lianh@jumei.com>
2020-06-18 17:56:18 +0800

    从首页开始跑。

--- dyData/driver.py
+++ dyData/driver.py
@@ -4,6 +4,7 @@ import random
 
 
 if __name__ == "__main__":
+    #从dy首页开始
     home = Element('dy_a70_home.xml')
     morehot = Element('dy_a70_morehot.xml')
     word_list = Element('dy_a70_word_list.xml')
@@ -20,13 +21,18 @@ if __name__ == "__main__":
 
 
     search_cord = home.findElementById('com.ss.android.ugc.aweme:id/amj')
+    event.touch(search_cord[0], search_cord[1])#点击搜索
+    time.sleep(round(random.uniform(0,9),2))
+
     morehot_cord = morehot.findElementById('com.ss.android.ugc.aweme:id/b88')
+    event.touch(morehot_cord[0], morehot_cord[1])#点击查看更多热点
+    time.sleep(round(random.uniform(0,9),2))
+
     video_tab_cord = word_detail.findElementByName('视频')
     video_word_detail_back_cord = word_detail.findElementById('com.ss.android.ugc.aweme:id/adb')
     print(search_cord, morehot_cord, video_tab_cord, video_word_detail_back_cord)
     #查看热搜榜
     hot = word_list.findElementsById('com.ss.android.ugc.aweme:id/b8f')
-    print(len(hot),hot)
     for i in range(len(hot)):
         x = hot[i][0]
         y = hot[i][1]
@@ -38,4 +44,11 @@ if __name__ == "__main__":
         time.sleep(round(random.uniform(0,9),2))
         event.touch(video_word_detail_back_cord[0],video_word_detail_back_cord[1])
         time.sleep(round(random.uniform(0,9),2))
+
+    #回到查看更多热点
+    event.touch(video_word_detail_back_cord[0],video_word_detail_back_cord[1])
+
+    #回到首页。
+    event.touch(video_word_detail_back_cord[0],video_word_detail_back_cord[1])
+
 
